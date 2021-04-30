dd11ec6753a973698d9cee247762b56fe536fc1b
Brad Rydzewski <bradley.rydzewski@harness.io>
2021-04-29 21:15:20 -0400

    update fragments to use css modules

--- src/fragments/login/Login.tsx
+++ src/fragments/login/Login.tsx
@@ -1,92 +1,25 @@
+// Copyright 2021 Harness, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // @ts-nocheck
 
 import { useRef } from "react";
-import { css, cx } from "@emotion/css";
 import Input from "../../components/input";
 import Harness from "../../assets/logos/HarnessLarge";
 
-const rootCss = css`
-	input {
-		width: 100%;
-		margin-bottom: 24px;
-
-		box-shadow: inset 0 0.5px 4px rgb(96 97 112 / 10%);
-		border: 1px solid #b0b1c3;
-		border-radius: 8px;
-		height: 40px;
-		padding: 0 9pt;
-	}
-
-	h2 {
-		font-weight: 600;
-		font-size: 32px;
-		line-height: 36px;
-		margin-top: 0px;
-		margin-bottom: 32px;
-		user-select: none;
-
-		small {
-			display: block;
-			text-transform: lowercase;
-			font-weight: 400;
-			font-size: 16px;
-		}
-	}
-`;
-
-const logoCss = css`
-	appearance: none;
-	background: none;
-	border: none;
-	cursor: pointer;
-	margin: 0px;
-	margin-bottom: 60px;
-	padding: 0px;
-	user-select: none;
-
-	svg {
-		height: 36px;
-	}
-`;
-
-const registerCss = css`
-	margin-top: 64px;
-	font-size: 15px;
-	user-select: none;
-
-	button {
-		appearance: none;
-		border: none;
-		background: transparent;
-		font-size: 15px;
-		font-family: Inter;
-		padding: 0px;
-		margin: 0px;
-		cursor: pointer;
-		color: #0278d5;
-	}
-`;
-
-const loginCss = css`
-	background: #0278d5;
-	box-shadow: 0 0 1px rgb(40 41 61 / 4%), 0 2px 4px rgb(96 97 112 / 16%);
-	border-radius: 8px;
-	height: 40px;
-	color: #fff;
-	border: none;
-	width: 100%;
-	padding: 0 9pt;
-	text-transform: uppercase;
-	font-size: 14px;
-	font-family: Inter;
-	user-select: none;
-	cursor: pointer;
-	transition: background-color 0.1s ease-in;
-
-	&:hover {
-		background: #04aae4;
-	}
-`;
+// @ts-ignore
+import styles from "./Login.module.css";
 
 export default (props) => {
 	const usernameElem = useRef(null);
@@ -101,8 +34,8 @@ export default (props) => {
 		}
 	};
 	return (
-		<div className={rootCss}>
-			<button className={logoCss} onClick={props.onLogo} alt="logo">
+		<div className={styles.root}>
+			<button className={styles.logo} onClick={props.onLogo} alt="logo">
 				<Harness />
 			</button>
 			<h2>
@@ -124,10 +57,10 @@ export default (props) => {
 					placeholder="Password"
 				/>
 			</div>
-			<button className={loginCss} onClick={handleLogin}>
+			<button className={styles.login} onClick={handleLogin}>
 				Login
 			</button>
-			<div className={registerCss}>
+			<div className={styles.register}>
 				No Account? <button onClick={props.onRegister}>Get Started</button>
 			</div>
 		</div>
--- src/fragments/register/Register.tsx
+++ src/fragments/register/Register.tsx
@@ -1,94 +1,25 @@
+// Copyright 2021 Harness, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // @ts-nocheck
 
 import { useRef } from "react";
-import { css, cx } from "@emotion/css";
 import Input from "../../components/input";
 import Harness from "../../assets/logos/HarnessLarge";
 
-const rootCss = css`
-	input {
-		width: 100%;
-		margin-bottom: 24px;
-
-		box-shadow: inset 0 0.5px 4px rgb(96 97 112 / 10%);
-		border: 1px solid #b0b1c3;
-		border-radius: 8px;
-		height: 40px;
-		padding: 0 9pt;
-	}
-
-	h2 {
-		font-weight: 600;
-		font-size: 32px;
-		line-height: 36px;
-		margin-top: 0px;
-		margin-bottom: 32px;
-		user-select: none;
-
-		small {
-			display: block;
-			text-transform: lowercase;
-			font-weight: 400;
-			font-size: 16px;
-		}
-	}
-`;
-
-const logoCss = css`
-	appearance: none;
-	background: none;
-	border: none;
-	cursor: pointer;
-	margin: 0px;
-	margin-bottom: 60px;
-	padding: 0px;
-	user-select: none;
-
-	svg {
-		height: 36px;
-	}
-`;
-
-const signinCss = css`
-	align-items: center;
-	appearance: none;
-	background: none;
-	border: none;
-	color: #0278d5;
-	cursor: pointer;
-	display: flex;
-	font-size: 16px;
-	float: right;
-	margin-top: 8px;
-	text-decoration: none;
-	user-select: none;
-
-	svg {
-		margin-right: 5px;
-	}
-`;
-
-const registerCss = css`
-	appearance: none;
-	background: #0278d5;
-	box-shadow: 0 0 1px rgb(40 41 61 / 4%), 0 2px 4px rgb(96 97 112 / 16%);
-	border-radius: 8px;
-	height: 40px;
-	color: #fff;
-	border: none;
-	width: 100%;
-	padding: 0 9pt;
-	text-transform: uppercase;
-	font-size: 14px;
-	font-family: Inter;
-	user-select: none;
-	cursor: pointer;
-	transition: background-color 0.1s ease-in;
-
-	&:hover {
-		background: #04aae4;
-	}
-`;
+// @ts-ignore
+import styles from "./Register.module.css";
 
 export default (props) => {
 	const usernameElem = useRef(null);
@@ -104,8 +35,8 @@ export default (props) => {
 	};
 	return (
 		<>
-			<div className={rootCss}>
-				<button className={signinCss} onClick={props.onLogin}>
+			<div className={styles.root}>
+				<button className={styles.signin} onClick={props.onLogin}>
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						width="24"
@@ -122,7 +53,7 @@ export default (props) => {
 					</svg>
 					Sign in
 				</button>
-				<button className={logoCss} onClick={props.onLogo} alt="logo">
+				<button className={styles.logo} onClick={props.onLogo} alt="logo">
 					<Harness />
 				</button>
 				<h2>
@@ -144,7 +75,7 @@ export default (props) => {
 						placeholder="Password"
 					/>
 				</div>
-				<button className={registerCss} onClick={handleRegister}>
+				<button className={styles.register} onClick={handleRegister}>
 					Register
 				</button>
 			</div>
