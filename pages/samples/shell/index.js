119ab27e9166d0b25c593b618e0b5330c84daad0
Brad Rydzewski <bradley.rydzewski@harness.io>
2021-08-10 21:35:23 -0400

    fix stroke-width linting error

--- pages/samples/shell/index.js
+++ pages/samples/shell/index.js
@@ -5,7 +5,7 @@ import { Header } from "../../../src/components/header/Header";
 import { Avatar } from "../../../src/components/avatar/Avatar";
 import { SideBar } from "../../../src/components/sidebar/Sidebar";
 import { css } from "@emotion/css";
-import { UtilityBar, UtilityBarButton } from "../../../src/components/utilitybar/utilitybar";
+import { UtilityBar, UtilityBarButton } from "../../../src/components/utilitybar/Utilitybar";
 
 const contentCss = css`
 max-width: 1100px;
@@ -100,5 +100,5 @@ const BellIcon = () => (
 )
 
 const SearchIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
 )
